8aa7fbdfcbb52e1cad96d804274093d961e39fab
Julio <jquintana1801@gmail.com>
2019-05-20 21:25:55 -0300

    Adding materia-ui and react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,12 +12,13 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           <Button variant="contained" color="primary">
+            Learn React
+          </Button>
         </a>
       </header>
     </div>
